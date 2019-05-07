aae0e8d0e0f7128b72be5472b39feecfef4cb844
Steve Cusack <stephen.cusack@mandg.com>
2019-05-07 11:47:37 +0100

    Refactor

--- ATP.StringCalculatorKata.Tests/StringCalculatorShould.cs
+++ ATP.StringCalculatorKata.Tests/StringCalculatorShould.cs
@@ -8,27 +8,35 @@ namespace ATP.StringCalculatorKata.Tests
     [TestFixture]
     public class StringCalculatorShould
     {
+        private StringCalculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new StringCalculator();
+        }
+
         [Test]
         public void GivenEmptyString_Return0()
         {
-            var  calculator = new StringCalculator();
             var result = calculator.Add("");
+
             result.Should().Be(0);
         }
 
         [Test]
         public void GivenStringOf1_Return1()
         {
-            var calculator = new StringCalculator();
             var result = calculator.Add("1");
+
             result.Should().Be(1);
         }
 
         [Test]
         public void GivenStringOf100_Return100()
         {
-            var calculator = new StringCalculator(); 
             var result = calculator.Add("100");
+
             result.Should().Be(100);
         }
     }
