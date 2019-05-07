7eb0538ae907a653c39137bd12aa491926a8a6a9
Steve Cusack <stephen.cusack@mandg.com>
2019-05-07 11:18:32 +0100

    Add project files.

---
+++ ATP.StringCalculatorKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATP.StringCalculatorKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATP.StringCalculatorKata.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b979d2f-abb6-4060-a5aa-c839fb96864a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ATP.StringCalculatorKata.Tests/StringCalculatorShould.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ATP.StringCalculatorKata.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringCalculatorShould
+    {
+        [Test]
+        public void GivenEmptyString_Return0()
+        {
+            var  calculator = new StringCalculator();
+            var result = calculator.Add("");
+            result.Should().Be(0);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
